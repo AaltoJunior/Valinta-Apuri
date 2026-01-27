1b630fb0835a96b8d6ca065de2c7025b6eb8f833
Jussi Roos <jussi.roos@gmail.com>
2026-01-27 12:20:40 +0200

    New line on dp auth link. Style.css aadded max-width for vertical overflow

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def initialize_dropbox_interactive():
     auth_flow = DropboxOAuth2FlowNoRedirect(APP_KEY, use_pkce=True, token_access_type='offline')
 
     authorize_url = auth_flow.start()
-    print("1. Go to: " + authorize_url)
+    print("1. Go to: \n" + authorize_url)
     print("2. Click \"Allow\" (you might have to log in first).")
     print("3. Copy the authorization code.")
     # auth_code = input("Enter the authorization code here: ").strip()
